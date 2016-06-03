fa8ee5fbb38774996951707b85e4df8ee8ed3f2f
Zane Sterling <zsterling@google.com>
2016-06-03 13:09:14 -0700

    Allow snip to be offset arbitrarily from match

--- source/app.d
+++ source/app.d
@@ -3,26 +3,45 @@ import std.regex;
 import std.stdio;
 
 int main(string[] args) {
-	bool snipAfter;
+	int offset = 0;
 	auto helpInfo = getopt(
 		args,
-		"after|a", &snipAfter
+		"offset|o", "Offset from match to start snipping", &offset,
 	);
 
-	if (args.length != 2) {
+	if (args.length != 2 || helpInfo.helpWanted) {
 		usage(args[0]);
-		return 1;
+		return args.length != 2 ? 1 : 0;
 	}
 
 	auto r = regex(args[1]);
-	foreach (line; stdin.byLine) {
-		if (line.matchFirst(r)) {
-			if (snipAfter) {
-				writeln(line);
+	if (offset < 0) {
+		char[][] linesBuf;
+		linesBuf.length = -offset;
+		size_t index = 0;
+		foreach (line; stdin.byLine) {
+			if (line.matchFirst(r)) {
+				break;
 			}
-			return 0;
+			if (linesBuf[index] !is null) {
+				writeln(linesBuf[index]);
+			}
+			linesBuf[index] = line;
+			index = (index + 1) % (-offset);
 		}
-		writeln(line);
+	} else {
+		foreach (line; stdin.byLine) {
+			if (line.matchFirst(r)) {
+				if (offset > 0) {
+					writeln(line);
+				}
+				break;
+			}
+			writeln(line);
+		}
+		foreach (i; 0 .. offset - 1) {
+			write(stdin.readln());
+		}
 	}
 
 	return 0;
